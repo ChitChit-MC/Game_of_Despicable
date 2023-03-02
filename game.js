bf9a470b4879ed81755f3615d4c1ac864365cbfd
Chit Chit M.C Zaw <chitchitmyetchealzaw@gmail.com>
2023-03-02 16:21:00 -0500

    made a memory game

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //card options
   const cardArray = [
     {
       name: "Gru",
@@ -73,9 +74,10 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/Dr.Nefario.jpg",
     },
   ];
+  //sort everything in the array randomly
   cardArray.sort(() => 0.5 - Math.random());
 
-  const grid = document.querySelector(".grid");
+  const grid = document.querySelector(".grid"); //grid display
   const resultDisplay = document.querySelector("#result");
   let cardsChosen = [];
   let cardsChosenId = [];
@@ -83,18 +85,28 @@ document.addEventListener("DOMContentLoaded", () => {
   //create your board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement("img");
-      card.setAttribute("src", "images/blank.jpg");
-      card.setAttribute("data-id", i);
-      card.setAttribute("style", "max-height:100px;max-width:100px;");
+      const card = document.createElement("img"); //create an image element
+      card.setAttribute("src", "images/blank.jpg"); //adding src attribute to an image
+      card.setAttribute("data-id", i); //adding data id
+      card.setAttribute("style", "height:100px; width:100px;");
       card.addEventListener("click", flipCard);
-      grid.appendChild(card);
+      grid.appendChild(card); //added 18 cards to grid
     }
   }
-
+  //flip your card-> it tells the user which card he clicks on
+  function flipCard() {
+    let cardId = this.getAttribute("data-id"); //this.getAttribute-> get the id of the card that the user clicked
+    cardsChosen.push(cardArray[cardId].name); //push the item into the (chosen) array
+    cardsChosenId.push(cardId); //push the cardId into cardsChosenID
+    this.setAttribute("src", cardArray[cardId].img); //show the image of the card that the user clicked
+    //we want to check 2 items in 500ms
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
   //check for matches
   function checkForMatch() {
-    const cards = document.querySelectorAll("img");
+    const cards = document.querySelectorAll("img"); //look for all the images->if u wanna add more images u can do (.grid images)
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
 
@@ -105,31 +117,20 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (cardsChosen[0] === cardsChosen[1]) {
       alert("You found a match");
       cards[optionOneId].setAttribute("src", "images/white.jpg");
-      cards[optionTwoId].setAttribute("src", "images/white.jpg");
+      cards[optionTwoId].setAttribute("src", "images/white.jpg"); //if its a match we add white images
       cards[optionOneId].removeEventListener("click", flipCard);
-      cards[optionTwoId].removeEventListener("click", flipCard);
-      cardsWon.push(cardsChosen);
+      cards[optionTwoId].removeEventListener("click", flipCard); //remove the ability to click on the card
+      cardsWon.push(cardsChosen); //recording how many cards/matches we have won
     } else {
       cards[optionOneId].setAttribute("src", "images/blank.jpg");
-      cards[optionTwoId].setAttribute("src", "images/blank.jpg");
+      cards[optionTwoId].setAttribute("src", "images/blank.jpg"); //flip back the card
       alert("Sorry, try again");
     }
     cardsChosen = [];
-    cardsChosenId = [];
+    cardsChosenId = []; // we can start the whole process again
     resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length / 2) {
       resultDisplay.textContent = "Congratulations! You found them all!";
-    }
-  }
-
-  //flip your card
-  function flipCard() {
-    let cardId = this.getAttribute("data-id");
-    cardsChosen.push(cardArray[cardId].name);
-    cardsChosenId.push(cardId);
-    this.setAttribute("src", cardArray[cardId].img);
-    if (cardsChosen.length === 2) {
-      setTimeout(checkForMatch, 500);
     }
   }
 
