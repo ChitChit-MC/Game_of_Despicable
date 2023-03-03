df6a36e0b1c99a42bec21b152c74021c47b70094
Chit Chit M.C Zaw <chitchitmyetchealzaw@gmail.com>
2023-03-02 21:59:12 -0500

    Add comments

--- game.js
+++ game.js
@@ -74,10 +74,10 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/Dr.Nefario.jpg",
     },
   ];
-  //sort everything in the array randomly
-  cardArray.sort(() => 0.5 - Math.random());
+  
+  cardArray.sort(() => 0.5 - Math.random());                    //sort everything randomly in the array 
 
-  const grid = document.querySelector(".grid"); //grid display
+  const grid = document.querySelector(".grid");                 //display grid
   const resultDisplay = document.querySelector("#result");
   let cardsChosen = [];
   let cardsChosenId = [];
@@ -85,28 +85,28 @@ document.addEventListener("DOMContentLoaded", () => {
   //create your board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement("img"); //create an image element
-      card.setAttribute("src", "images/blank.jpg"); //adding src attribute to an image
-      card.setAttribute("data-id", i); //adding data id
+      const card = document.createElement("img");               //create an image element
+      card.setAttribute("src", "images/blank.jpg");             //add src attribute to an image
+      card.setAttribute("data-id", i);                          //add data id
       card.setAttribute("style", "height:100px; width:100px;");
       card.addEventListener("click", flipCard);
-      grid.appendChild(card); //added 18 cards to grid
+      grid.appendChild(card);                                   //add 18 cards to grid
     }
   }
-  //flip your card-> it tells the user which card he clicks on
+  //flip your card
   function flipCard() {
-    let cardId = this.getAttribute("data-id"); //this.getAttribute-> get the id of the card that the user clicked
-    cardsChosen.push(cardArray[cardId].name); //push the item into the (chosen) array
-    cardsChosenId.push(cardId); //push the cardId into cardsChosenID
-    this.setAttribute("src", cardArray[cardId].img); //show the image of the card that the user clicked
-    //we want to check 2 items in 500ms
+    let cardId = this.getAttribute("data-id");                  //get the id of the card that the user clicked
+    cardsChosen.push(cardArray[cardId].name);                   //push the item into the (chosen) array
+    cardsChosenId.push(cardId);                                 //push the cardId into cardsChosenID
+    this.setAttribute("src", cardArray[cardId].img);            //show the image of the card that the user clicked
+    
     if (cardsChosen.length === 2) {
       setTimeout(checkForMatch, 500);
     }
   }
   //check for matches
   function checkForMatch() {
-    const cards = document.querySelectorAll("img"); //look for all the images->if u wanna add more images u can do (.grid images)
+    const cards = document.querySelectorAll("img");             //look for all the images->If u wanna add more images u can do (.grid images)
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
 
@@ -117,17 +117,17 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (cardsChosen[0] === cardsChosen[1]) {
       alert("You found a match");
       cards[optionOneId].setAttribute("src", "images/white.jpg");
-      cards[optionTwoId].setAttribute("src", "images/white.jpg"); //if its a match we add white images
+      cards[optionTwoId].setAttribute("src", "images/white.jpg");     //add white images if it is a match
       cards[optionOneId].removeEventListener("click", flipCard);
-      cards[optionTwoId].removeEventListener("click", flipCard); //remove the ability to click on the card
-      cardsWon.push(cardsChosen); //recording how many cards/matches we have won
+      cards[optionTwoId].removeEventListener("click", flipCard);      //remove the ability to click on the card
+      cardsWon.push(cardsChosen);                                     //record how many cards/matches we have won
     } else {
       cards[optionOneId].setAttribute("src", "images/blank.jpg");
-      cards[optionTwoId].setAttribute("src", "images/blank.jpg"); //flip back the card
+      cards[optionTwoId].setAttribute("src", "images/blank.jpg");     //flip the card back
       alert("Sorry, try again");
     }
     cardsChosen = [];
-    cardsChosenId = []; // we can start the whole process again
+    cardsChosenId = [];                                               //start the whole process again
     resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length / 2) {
       resultDisplay.textContent = "Congratulations! You found them all!";
